9f9395b8f44baf688ffa8b708ae916935b893a2a
Joe Abele <josephabele2@gmail.com>
2020-08-10 16:41:52 -0400

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,27 @@
 import React, {Component} from "react"
+import List from "./List";
 
+const ASSETS ={
+    doggy: require('../assets/doggy.jpg'),
+    
+};
 
 
 class Home extends Component{
     render(){
 
         return(
-            <div>
+            <div className="home-page">
+                <h1 className ="title">Paw Daddy Paw Cream</h1>
+                <h2 className ="sub-title">#1 Vet Recommend Paw Cream</h2>
+                <div className="divider"></div>
+                <img
+                src= {ASSETS.doggy}
+                class="dog-photo"
+                alt="dog-photo"
+                />
+         <div className="start-button"><p className="button-title">BUY CREAM</p></div>
+          <List />      
          
             </div>
         )
