b8ab9646223d94b03fd393f0d2a40e422ce14752
Joe Abele <josephabele2@gmail.com>
2020-09-01 15:36:42 -0400

    Updates

---
+++ precache-manifest.f1bcff8eeb5b36e951b3e317ec92944d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce5d2718c8fe92394ee72501c168201",
+    "url": "/dog/index.html"
+  },
+  {
+    "revision": "8a526f15909f6b599a8a",
+    "url": "/dog/static/css/main.28772848.chunk.css"
+  },
+  {
+    "revision": "6f220533f0e068b691ed",
+    "url": "/dog/static/js/2.70ea7512.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dog/static/js/2.70ea7512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a526f15909f6b599a8a",
+    "url": "/dog/static/js/main.ff9bd64f.chunk.js"
+  },
+  {
+    "revision": "7a009c807a17b07fdd9c",
+    "url": "/dog/static/js/runtime-main.d9caf453.js"
+  },
+  {
+    "revision": "6c63b87ad6c637b9ae8aa88763cf13ac",
+    "url": "/dog/static/media/blood.6c63b87a.jpg"
+  },
+  {
+    "revision": "683651c72fc8d7431a6019ce3b13c1f6",
+    "url": "/dog/static/media/brush.683651c7.jpg"
+  },
+  {
+    "revision": "3ce3a1f80cb48e043934a912251d9534",
+    "url": "/dog/static/media/doggy.3ce3a1f8.jpg"
+  },
+  {
+    "revision": "d2ac399c2d8c747e48263849b54c2363",
+    "url": "/dog/static/media/fine.d2ac399c.jpg"
+  },
+  {
+    "revision": "c0cde466c409896588693a007ac8a418",
+    "url": "/dog/static/media/jam.c0cde466.jpg"
+  },
+  {
+    "revision": "34a4f31c87f0e3f9e7c682ebf7eb3664",
+    "url": "/dog/static/media/left-arrow.34a4f31c.svg"
+  },
+  {
+    "revision": "2b9492e285a12481c2ac0c2731b89213",
+    "url": "/dog/static/media/mandog.2b9492e2.jpg"
+  },
+  {
+    "revision": "3ca0861f7b638166097d5158969ca4c2",
+    "url": "/dog/static/media/paw-no-title.3ca0861f.jpg"
+  },
+  {
+    "revision": "29ec74860cd06f9a49bf9da9e700cedb",
+    "url": "/dog/static/media/paw3.29ec7486.jpg"
+  },
+  {
+    "revision": "0f5c3c2a8d951ff0109df6f891d190cf",
+    "url": "/dog/static/media/reddit.0f5c3c2a.png"
+  },
+  {
+    "revision": "5e5b72cc72ffd2c9ea04f8a4742dd6b1",
+    "url": "/dog/static/media/right-arrow.5e5b72cc.svg"
+  },
+  {
+    "revision": "5419d8feb325e1843f2f02b07f13ea57",
+    "url": "/dog/static/media/shirt.5419d8fe.jpg"
+  },
+  {
+    "revision": "7e7a22a77f9cf4e1159b8c68826e9394",
+    "url": "/dog/static/media/twitter.7e7a22a7.png"
+  }
+]);
